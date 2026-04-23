43194126971e97962b348a2bc1d2de119ce9f986
SamanthaClaes <Samantha.claes@student.hepl.be>
2026-04-23 15:30:54 +0200

    put picture on home page

--- resources/views/pages/⚡home.blade.php
+++ resources/views/pages/⚡home.blade.php
@@ -2,15 +2,20 @@
 
 use Livewire\Component;
 
-new class extends Component
-{
+new class extends Component {
     //
 };
 ?>
 
 <div>
-    <section>
-        <h1>Test</h1>
-        <p>Bonjour</p>
+    <section class="relative w-full h-[60vh] overflow-hidden">
+        <img src="{{ asset('img/Hero_home.jpeg') }}" alt="pleins de chiens en balade"
+             class="absolute inset-0 w-full h-full object-cover">
+        <div class="absolute inset-0 bg-black/60"></div>
+        <div class="absolute inset-0 flex flex-col justify-center items-center gap-4">
+            <h1 class="
+               text-white text-6xl font-bold uppercase z-10">Paw club</h1>
+            <span class=" text-white text-3xl z-10">Un service de garde fiable et attentionné pour accompagner votre animal en toute sérénité.</span>
+        </div>
     </section>
 </div>
