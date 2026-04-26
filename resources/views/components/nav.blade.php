8e768b40ffc2f540bd3fb55db65a1c25a762f77d
SamanthaClaes <Samantha.claes@student.hepl.be>
2026-04-26 16:08:51 +0200

    make card petsitter responsive and add component form

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,21 +1,35 @@
 <nav class="bg-element p-2 text-text">
     <ul class="flex gap-8 justify-around items-center ">
+        <input type="checkbox" id="menu-toggle" class="peer hidden">
+        <label for="menu-toggle"
+               class="md:hidden flex items-center px-3 py-2 border rounded text-text-brown cursor-pointer">
+            <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+            </svg>
+        </label>
+        <div class="w-full hidden peer-checked:block md:flex md:items-center md:w-auto">
+            <div class="text-sm md:grow flex flex-col md:flex-row md:items-center">
+                <li>
+                    <a href="{{ route('daycare.index') }}" class="hover:underline">La garderie</a>
+                </li>
+                <li>
+                    <a href="{{ route('petsitter.index') }}" class="hover:underline">Les petsitters</a>
+                </li>
+                <li>
+                    <a href="/" class="hover:underline">Contact</a>
+                </li>
+                <li>
+                    <a href="/" class="hover:underline">Me connecter</a>
+                </li>
+            </div>
+        </div>
         <li>
             <a href="{{ route('home')}}">
                 <x-svg.logo/>
             </a>
         </li>
-        <li>
-            <a href="{{ route('daycare.index') }}" class="hover:underline">La garderie</a>
-        </li>
-        <li>
-            <a href="{{ route('petsitter.index') }}" class="hover:underline">Les petsitters</a>
-        </li>
-        <li>
-            <a href="/" class="hover:underline">Contact</a>
-        </li>
-        <li>
-            <a href="/" class="hover:underline">Me connecter</a>
-        </li>
+
+
     </ul>
+
 </nav>
--- resources/views/pages/⚡home.blade.php
+++ resources/views/pages/⚡home.blade.php
@@ -42,7 +42,7 @@ new class extends Component { //on peut mettre #[Title('title')] entre le new et
                     class="w-full max-w-sm sm:max-w-md lg:max-w-87.5 h-auto rounded-lg object-cover"
                 >
 
-                <p class=" line-clamp-5 md:text-text text-sm  max-w-2xl mb-5 text-center lg:line-clamp-none lg:text-left">
+                <p class=" line-clamp-5 md:text-text text-sm  max-w-2xl mb-5 text-center lg:line-clamp-none lg:text-left lg:text-lg lg:leading-relaxed">
                     PawClub est un espace de garderie dédié exclusivement aux chiens,
                     offrant un environnement sécurisé, encadré et adapté à leurs besoins.
                     Chaque animal y est accueilli avec attention, dans un cadre pensé pour
@@ -61,54 +61,102 @@ new class extends Component { //on peut mettre #[Title('title')] entre le new et
                     src="{{ asset('svg/illu_1.svg') }}"
                     alt="une femme et un homme jouant au frisbee avec un chien"
                     class="
-            pointer-events-none
             absolute
             bottom-0 right-0
             w-30 sm:w-40 md:w-56 lg:w-64 xl:w-72
             translate-x-1/4 translate-y-1/4">
             </div>
-            <div class="flex justify-center">
-                <a href="#" class=" text-cta  font-bold uppercase bg-card-green hover:bg-hover hover:text-white p-5 lg:w-1/2 rounded-lg mb-6  text-center">Découvrir nos petsitters</a>
+            <div class="flex justify-center mt-6">
+                <a href="#"
+                   class=" text-cta  font-bold uppercase bg-card-green hover:bg-hover hover:text-white p-5 lg:w-1/2 rounded-lg mb-6  text-center shadow-md/10">Découvrir
+                    nos petsitters</a>
             </div>
 
         </div>
     </section>
     <section>
-        <h2 class="text-center text-2xl font-extrabold text-text lg:text-5xl lg:mb-6">Nos services</h2>
-        <div class="px-6 py-6 lg:flex gap-8">
-            <div class="border-5 mx-auto max-w-6xl border-card-orange rounded-lg sm:px-6 lg:px-8 py-8">
-                <h3 class="text-lg mt-4 mb-4 lg:mt-8 lg:mb-8 text-center lg:text-4xl text-text font-bold">Notre garderie</h3>
-                <div>
-                    <p class="text-sm leading-8 line-clamp-7 text-text px-10 md:text-text  mb-5 text-center lg:text-left lg:line-clamp-none">
-                        Dans notre garderie, vos chiens sont accueillis
-                        dans un espace
-                        sécurisé et encadré, où ils profitent d’attention, de jeux et de moments de socialisation tout
-                        au long de la journée, en présence d’une équipe attentive à leur bien-être. Chaque accueil est
-                        pensé pour répondre à leurs besoins, en tenant compte de leur rythme, de leur caractère et de
-                        leurs habitudes. Notre objectif est de leur offrir un environnement rassurant et stimulant, afin
-                        qu’ils puissent s’épanouir pleinement pendant votre absence.</p>
+        <div class="relative">
+
+            <h2 class="text-center text-2xl font-extrabold text-text lg:text-5xl lg:mb-10">
+                Nos services
+            </h2>
+
+            <div class="px-6 py-6 lg:flex lg:items-stretch lg:gap-10 lg:max-w-6xl lg:mx-auto">
+
+                <div
+                    class="relative border-4 w-full lg:w-1/2 border-card-orange rounded-lg sm:px-6 lg:px-8 py-8 mb-10 lg:mb-0">
+
+                    <h3 class="text-lg mt-4 mb-4 lg:mt-8 lg:mb-8 text-center lg:text-3xl text-text font-bold">
+                        Notre garderie
+                    </h3>
+
+                    <p class="text-sm leading-8 line-clamp-7 text-text px-6 mb-5 text-center lg:text-left lg:line-clamp-none">
+                        Dans notre garderie, vos chiens sont accueillis dans un espace sécurisé et encadré, où ils
+                        profitent d’attention, de jeux et de moments de socialisation tout au long de la journée, en
+                        présence d’une équipe attentive à leur bien-être. Chaque accueil est pensé pour répondre à leurs
+                        besoins, en tenant compte de leur rythme, de leur caractère et de leurs habitudes. Notre
+                        objectif est de leur offrir un environnement rassurant et stimulant, afin qu’ils puissent
+                        s’épanouir pleinement pendant votre absence.
+                    </p>
+
+                    <img
+                        src="{{ asset('svg/icons 1.svg') }}"
+                        alt="garçon qui joue à la balle avec son chien"
+                        class="
+                        pointer-events-none
+                        absolute
+                        bottom-0 right-0
+                        w-40 sm:w-32 md:w-40 lg:w-70
+                        translate-x-1/4 translate-y-1/4
+                    "
+                    >
+
+                    <div class="flex justify-center">
+                        <a href="#"
+                           class="text-text-orange font-bold uppercase bg-card-orange hover:bg-hover-orange hover:text-white p-5 lg:w-2/3 rounded-lg mb-6 shadow-md/10 text-center">
+                            Réserver une garde
+                        </a>
+                    </div>
+
                 </div>
-                <div class="flex justify-center">
-                    <a href="#" class=" text-text-orange  font-bold uppercase bg-card-orange hover:bg-hover-orange hover:text-white p-5 lg:w-1/2 rounded-lg mb-6  text-center">Découvrir nos petsitters</a>
-                </div>
-             {{--   <div class="absolute">
-                    <img src="{{ asset('svg/icons 1.svg') }}" alt="" class="relative bottom-45 left-135">
-                </div>--}}
-            </div>
-            <div>
-                <h3>Nos petsitters</h3>
-                <div>
-                    <p>Nos petsitters s’occupent de vos animaux en fonction de leurs besoins spécifiques et des
+
+                <div class="relative border-4 w-full lg:w-1/2 border-card-pink rounded-lg sm:px-6 lg:px-8 py-8">
+
+                    <h3 class="text-lg mt-4 mb-4 lg:mt-8 lg:mb-8 text-center lg:text-3xl text-text font-bold">
+                        Nos petsitters
+                    </h3>
+
+                    <p class="text-sm leading-8 line-clamp-6 text-text px-6 mb-5 text-center lg:text-left lg:line-clamp-none">
+                        Nos petsitters s’occupent de vos animaux en fonction de leurs besoins spécifiques et des
                         informations renseignées dans leur fiche, afin d’assurer une prise en charge adaptée et
                         personnalisée.
                         Chaque petsitter définit en amont les types d’animaux qu’il peut accueillir ainsi que ses
                         disponibilités, garantissant ainsi une correspondance cohérente avec votre demande. Grâce à
                         cette organisation, vos compagnons bénéficient d’une attention particulière et d’un
-                        environnement adapté, en toute confiance.</p>
+                        environnement adapté, en toute confiance.
+                    </p>
+                    <img
+                        src="{{ asset('svg/illu_3.svg') }}"
+                        alt="garçon qui carresse un chat"
+                        class="
+                        pointer-events-none
+                        absolute
+                        bottom-0 right-0
+
+                        w-20 sm:w-32 md:w-40 lg:w-48
+                        translate-x-1/4 translate-y-1/4
+                    "
+                    >
+
+                    <div class="flex justify-center">
+                        <a href="#"
+                           class="shadow-md/10 text-text-pink font-bold uppercase bg-card-pink hover:bg-hover-pink hover:text-white p-5 lg:w-2/3 rounded-lg mb-6 text-center">
+                            Réserver un petsitter
+                        </a>
+                    </div>
+
                 </div>
-                <div>
-                    <a href="">Réserver un petsitter</a>
-                </div>
+
             </div>
         </div>
     </section>
