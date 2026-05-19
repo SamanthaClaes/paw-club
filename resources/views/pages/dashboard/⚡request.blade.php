543a9135a5d50483d55d34026eed220b7d7eaa7d
SamanthaClaes <Samantha.claes@student.hepl.be>
2026-05-19 23:12:27 +0200

    create modale owner

--- resources/views/pages/dashboard/⚡request.blade.php
+++ resources/views/pages/dashboard/⚡request.blade.php
@@ -4,11 +4,13 @@ use App\Models\DayCareRequest;
 use App\Models\User;
 use Carbon\Carbon;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 new #[Layout('layouts::dashboard', ['title' => 'Demandes de garde'])]
 class extends Component {
     public $requests = [];
+    public ?User $selectedOwner = null;
 
     public function mount(): void
     {
@@ -18,6 +20,16 @@ class extends Component {
             'pet.animalType',
             'pet.breed',
         ])->get();
+    }
+
+    #[On('open-owner-modal')]
+    public function loadOwner($userId): void
+    {
+        $this->selectedOwner = User::with([
+            'pets',
+            'pets.breed',
+            'pets.animalType',
+        ])->findOrFail($userId);
     }
 };
 ?>
@@ -29,9 +41,7 @@ class extends Component {
     </section>
     @foreach($requests as $request)
         <section class="border-5 border-stroke rounded-md overflow-hidden bg-card w-full mt-6 lg:max-w-3xl ml-25">
-
             <div class="flex flex-col sm:flex-row h-full">
-
                 <div class="w-full h-64 sm:h-auto sm:w-1/3">
                     <img
                         src="{{ \Illuminate\Support\Facades\Storage::url($request->pet->pet_image) }}"
@@ -41,9 +51,12 @@ class extends Component {
                 </div>
 
                 <div class="w-full sm:w-2/3 p-4 sm:p-6 flex flex-col justify-between">
-
+                    <div class="flex justify-end  underline">
+                        <button wire:click="$dispatch('open-owner-modal', { userId: {{ $request->user->id }} })"
+                                class="cursor-pointer mb-6">Voir la fiche du propriétaire
+                        </button>
+                    </div>
                     <div>
-
                         <h1 class="uppercase font-extrabold text-text text-lg sm:text-xl mb-4 sm:mb-6">
                             Informations de {{ $request->pet->name }}
                         </h1>
@@ -97,8 +110,83 @@ class extends Component {
                 </div>
 
             </div>
-
         </section>
+        <dialog
+        wire:ignore.self
+        x-data="{ open: false }"
 
+        x-on:open-owner-modal.window="
+        open = true;
+        $el.showModal();
+        "
+
+        x-on:close="
+        open = false;
+        "
+
+        x-cloak
+        class="rounded-2xl p-0 backdrop:bg-black/50 w-full mx-auto mt-20 max-w-xl shadow-xl"
+        >
+
+            <div
+                x-show="open"
+                x-transition
+                @click.outside="
+            open = false;
+            $el.closest('dialog').close();
+        "
+                class="bg-white rounded-2xl p-8 relative"
+            >
+
+                <button
+                    type="button"
+                    @click="
+                open = false;
+                $el.closest('dialog').close();
+            "
+                    class="absolute top-4 right-4 text-3xl text-text hover:text-red-500 transition cursor-pointer"
+                >
+                    ×
+                </button>
+
+                <h2 class="text-2xl font-extrabold text-text uppercase mb-6">
+                    Information du propriétaire
+                </h2>
+                <p class="text-text text-lg mb-10">
+                    Prénom :
+                 <span class="text-text font-bold">{{ $selectedOwner?->first_name }}</span>
+                </p>
+                <p class="text-text text-lg mb-10">
+                    Nom :
+                   <span class="text-text font-bold"> {{ $selectedOwner?->last_name }}</span>
+                </p>
+                <p class="text-text text-lg mb-10">
+                    Email :
+                    <a href="mailto:{{ $selectedOwner?->email }}" class="font-bold">{{ $request->user->email }}</a>
+                </p>
+                <p class="text-text text-lg mb-10">
+                    Télépone :
+                    <a href="tel:{{ $selectedOwner?->phone }}" class="font-bold">{{ $request->user->phone }}</a>
+                </p>
+
+                <div class="flex justify-end gap-4">
+
+                    <button
+                        type="button"
+                        @click="
+                    open = false;
+
+                    $el.closest('dialog').close();
+                "
+                        class="border-2 border-gray-300 px-6 py-3 rounded-lg font-bold uppercase hover:bg-gray-100 transition cursor-pointer"
+                    >
+                        Fermer la modale
+                    </button>
+
+                </div>
+
+            </div>
+
+        </dialog>
     @endforeach
 </div>
