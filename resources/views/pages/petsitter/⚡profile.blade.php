b55c4b4edcc3aa0a7a444d2c32efb96cbeafdfae
SamanthaClaes <Samantha.claes@student.hepl.be>
2026-06-02 21:13:05 +0200

    test

--- resources/views/pages/petsitter/⚡profile.blade.php
+++ resources/views/pages/petsitter/⚡profile.blade.php
@@ -63,6 +63,10 @@ class extends Component {
                 $fileName,
                 's3'
             );
+            dd([
+                'path' => $path,
+                'exists' => Storage::disk('s3')->exists($path),
+            ]);
             ProcessImageJob::dispatchSync($fileName, $path);
             $validated['image'] = $path;
         }
